a5369956eb38fb9b37e28879f89efcd564b6f7c8
Carlos Afonso Pérez <carlos.afonso.perez@gmail.com>
2016-11-02 08:20:29 +0100

    Make example.py executable

--- example.py
+++ example.py
@@ -1,19 +1,23 @@
-from matplotlib.pylab import gca, figure, plot, subplot, title, xlabel, ylabel, xlim,show
+#!/usr/bin/env python
+
+from matplotlib.pylab import gca, figure, plot, subplot, title, xlabel, ylabel, xlim, show
 from matplotlib.lines import Line2D
 import segment
 import fit
 
-def draw_plot(data,plot_title):
-    plot(range(len(data)),data,alpha=0.8,color='red')
+
+def draw_plot(data, plot_title):
+    plot(range(len(data)), data, alpha=0.8, color='red')
     title(plot_title)
     xlabel("Samples")
     ylabel("Signal")
-    xlim((0,len(data)-1))
+    xlim((0, len(data) - 1))
+
 
 def draw_segments(segments):
     ax = gca()
     for segment in segments:
-        line = Line2D((segment[0],segment[2]),(segment[1],segment[3]))
+        line = Line2D((segment[0], segment[2]), (segment[1], segment[3]))
         ax.add_line(line)
 
 with open("example_data/16265-normalecg.txt") as f:
@@ -23,44 +27,48 @@ data = [float(x.split("\t")[2].strip()) for x in file_lines[100:320]]
 
 max_error = 0.005
 
-#sliding window with regression
+# sliding window with regression
 figure()
-segments = segment.slidingwindowsegment(data, fit.regression, fit.sumsquared_error, max_error)
-draw_plot(data,"Sliding window with regression")
+segments = segment.slidingwindowsegment(
+    data, fit.regression, fit.sumsquared_error, max_error)
+draw_plot(data, "Sliding window with regression")
 draw_segments(segments)
 
-#bottom-up with regression
+# bottom-up with regression
 figure()
-segments = segment.bottomupsegment(data, fit.regression, fit.sumsquared_error, max_error)
-draw_plot(data,"Bottom-up with regression")
+segments = segment.bottomupsegment(
+    data, fit.regression, fit.sumsquared_error, max_error)
+draw_plot(data, "Bottom-up with regression")
 draw_segments(segments)
 
-#top-down with regression
+# top-down with regression
 figure()
-segments = segment.topdownsegment(data, fit.regression, fit.sumsquared_error, max_error)
-draw_plot(data,"Top-down with regression")
+segments = segment.topdownsegment(
+    data, fit.regression, fit.sumsquared_error, max_error)
+draw_plot(data, "Top-down with regression")
 draw_segments(segments)
 
 
-
-#sliding window with simple interpolation
+# sliding window with simple interpolation
 figure()
-segments = segment.slidingwindowsegment(data, fit.interpolate, fit.sumsquared_error, max_error)
-draw_plot(data,"Sliding window with simple interpolation")
+segments = segment.slidingwindowsegment(
+    data, fit.interpolate, fit.sumsquared_error, max_error)
+draw_plot(data, "Sliding window with simple interpolation")
 draw_segments(segments)
 
-#bottom-up with  simple interpolation
+# bottom-up with  simple interpolation
 figure()
-segments = segment.bottomupsegment(data, fit.interpolate, fit.sumsquared_error, max_error)
-draw_plot(data,"Bottom-up with simple interpolation")
+segments = segment.bottomupsegment(
+    data, fit.interpolate, fit.sumsquared_error, max_error)
+draw_plot(data, "Bottom-up with simple interpolation")
 draw_segments(segments)
 
-#top-down with  simple interpolation
+# top-down with  simple interpolation
 figure()
-segments = segment.topdownsegment(data, fit.interpolate, fit.sumsquared_error, max_error)
-draw_plot(data,"Top-down with simple interpolation")
+segments = segment.topdownsegment(
+    data, fit.interpolate, fit.sumsquared_error, max_error)
+draw_plot(data, "Top-down with simple interpolation")
 draw_segments(segments)
 
 
 show()
-
